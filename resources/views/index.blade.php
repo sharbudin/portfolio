60e3838724e33bb134a8efe4d1b057d1d5216d99
Sharbudin <112697481+sharbudin@users.noreply.github.com>
2023-07-22 10:15:29 +0530

    favicon font

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>I am Sharbudin</title>
+  <title>I͎ ͎a͎m͎ ͎s͎h͎a͎r͎b͎u͎d͎i͎n͎</title>
 
   {{-- <link rel="icon" href="./assets/images/favicon.ico" type="image/x-icon"> --}}
 
